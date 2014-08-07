e6feaf09df9886061bc8b69acc6fdf6d9b2ee9d8
Mark Senff <mark@senff.com>
2014-08-07 11:04:26 -0400

    Near-release update

--- sticky-menu-or-anything.js
+++ sticky-menu-or-anything.js
@@ -1,27 +1,32 @@
+/**
+* @preserve Sticky Anything 0.91 | @senff | GPL2 Licensed
+* NOT READY FOR RELEASE YET.
+*/
+
 (function ($) {
  
     $.fn.stickOnTop = function() {
 
         // Create a clone of the menu, right next to original (in the DOM).
-        $(this).addClass('original').clone().insertAfter(this).addClass('cloned').css('position','fixed').css('top','0').css('margin-top','0').removeClass('original').hide();
+        $(this).addClass('original').clone().insertAfter(this).addClass('cloned').css('position','fixed').css('top','0').css('margin-top','0').css('z-index','500').removeClass('original').hide();
 
-        stickWhat = document.querySelector('.original');   // stickWhat = document.querySelector(this.selector); is deprecated now??
-
-        stickIt(stickWhat);
-        $(window).resize(function() {stickIt(stickWhat);});
-        $(window).scroll(function() {stickIt(stickWhat);});
+        stickIt();
+        $(window).resize(function() {stickIt();});
+        $(window).scroll(function() {stickIt();});
 
         return this;
     };
 
 
-    function stickIt(theElement) {
-        
-        var elementPos = theElement.offsetTop;
-        stickyTop = 0;
+    function stickIt() {
+              
+      var orgElementPos = $('.original').offset();
+      orgElementTop = orgElementPos.top;               
+
+      stickyTop = 0;
       
-       if (jQuery(window).scrollTop() >= (elementPos - stickyTop)) {
-         // scrolled past the menu; only show the cloned, sticky menu.
+      if ($(window).scrollTop() >= (orgElementTop - stickyTop)) {
+         // scrolled past the original position; now only show the cloned, sticky element.
          
          // Cloned element should always have same left position and width as original element.     
            orgElement = $('.original');
@@ -30,14 +35,13 @@
            widthOrgElement = orgElement.width();
   
        
-         jQuery('.cloned').css('left',leftOrgElement+'px').css('top',stickyTop+'px').css('width',widthOrgElement+'px').show();
-         jQuery('.original').css('visibility','hidden');
+         $('.cloned').css('left',leftOrgElement+'px').css('top',stickyTop+'px').css('width',widthOrgElement+'px').show();
+         $('.original').css('visibility','hidden');
        } else {
          // not scrolled past the menu; only show the original menu.
-         jQuery('.cloned').hide();
-         jQuery('.original').css('visibility','visible');
+         $('.cloned').hide();
+         $('.original').css('visibility','visible');
        }
     }
 
- 
 }(jQuery));
