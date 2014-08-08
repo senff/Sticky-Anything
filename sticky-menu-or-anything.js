b86b5ce362a90b7c3d7475ebf860dc43d6bd57da
Mark Senff <mark@senff.com>
2014-08-08 12:33:16 -0400

    Release version

--- sticky-menu-or-anything.js
+++ sticky-menu-or-anything.js
@@ -1,47 +1,56 @@
 /**
-* @preserve Sticky Anything 0.91 | @senff | GPL2 Licensed
-* NOT READY FOR RELEASE YET.
+* @preserve Sticky Anything 1.0 | @senff | GPL2 Licensed
 */
 
 (function ($) {
- 
-    $.fn.stickOnTop = function() {
 
-        // Create a clone of the menu, right next to original (in the DOM).
-        $(this).addClass('original').clone().insertAfter(this).addClass('cloned').css('position','fixed').css('top','0').css('margin-top','0').css('z-index','500').removeClass('original').hide();
+  $.fn.stickThis = function(options) {
 
-        stickIt();
-        $(window).resize(function() {stickIt();});
-        $(window).scroll(function() {stickIt();});
+    var settings = $.extend({
+      // Default
+      top: 0
+      }, options );
 
-        return this;
-    };
+    var numElements = $(this).length;
 
-
-    function stickIt() {
-              
-      var orgElementPos = $('.original').offset();
-      orgElementTop = orgElementPos.top;               
-
-      stickyTop = 0;
-      
-      if ($(window).scrollTop() >= (orgElementTop - stickyTop)) {
-         // scrolled past the original position; now only show the cloned, sticky element.
-         
-         // Cloned element should always have same left position and width as original element.     
-           orgElement = $('.original');
-           coordsOrgElement = orgElement.offset();
-           leftOrgElement = coordsOrgElement.left;  
-           widthOrgElement = orgElement.width();
-  
-       
-         $('.cloned').css('left',leftOrgElement+'px').css('top',stickyTop+'px').css('width',widthOrgElement+'px').show();
-         $('.original').css('visibility','hidden');
-       } else {
-         // not scrolled past the menu; only show the original menu.
-         $('.cloned').hide();
-         $('.original').css('visibility','visible');
-       }
+    if (numElements < 1) {
+      // There are no elements on the page with the called selector.
+    } else if (numElements > 1) {
+      // This is not going to work either. You can't make more than one element sticky. They will only get in eachother's way.
+      // Make sure that you use an selector that applies to only ONE SINGLE element on the page.
+      // Want to find out where all the elements are that you targeted? Uncomment the next line to debug.
+      // $(this).css('border','solid 3px #00ff00');
+    } else {
+      // Create a clone of the menu, right next to original (in the DOM).
+      $(this).addClass('original').clone().insertAfter(this).addClass('cloned').css('position','fixed').css('top','0').css('margin-top','0').css('z-index','500').removeClass('original').hide();
+      checkElement = setInterval(function(){stickIt(settings.top)},10);
     }
+
+    return this;
+  };
+
+
+function stickIt(stickyTop) {
+
+  var orgElementPos = $('.original').offset();
+  orgElementTop = orgElementPos.top;               
+
+  if ($(window).scrollTop() >= (orgElementTop - stickyTop)) {
+    // scrolled past the original position; now only show the cloned, sticky element.
+
+    // Cloned element should always have same left position and width as original element.     
+    orgElement = $('.original');
+    coordsOrgElement = orgElement.offset();
+    leftOrgElement = coordsOrgElement.left;  
+    widthOrgElement = orgElement.width();
+
+    $('.cloned').css('left',leftOrgElement+'px').css('top',stickyTop+'px').css('width',widthOrgElement+'px').show();
+    $('.original').css('visibility','hidden');
+  } else {
+    // not scrolled past the menu; only show the original menu.
+    $('.cloned').hide();
+    $('.original').css('visibility','visible');
+  }
+}
 
 }(jQuery));
